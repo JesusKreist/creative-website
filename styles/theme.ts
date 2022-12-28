cbf0a8080860add8f3a825b9da04849e21d78a7f
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-28 08:58:15 +0100

    Add theme file

---
+++ styles/theme.ts
@@ -0,0 +1,58 @@
+import { extendTheme } from "@chakra-ui/react";
+import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
+
+// const breakpoints = {
+//   sm: "320px",
+//   md: "768px",
+//   lg: "960px",
+//   xl: "1200px",
+//   "2xl": "1440px",
+// };
+
+// const brandPrimaryOnLight = defineStyle({
+//   background: "black",
+//   color: "white",
+//   border: "2px solid black",
+//   borderRadius: "none",
+//   padding: "2rem 3rem",
+//   fontSize: "xl",
+//   fontWeight: "bold",
+//   letterSpacing: "0.25ch",
+//   _hover: {
+//     background: "white",
+//     color: "black",
+//     border: "2px solid black",
+//   },
+// });
+
+// const brandPrimaryOnDark = defineStyle({
+//   color: "white",
+//   border: "2px solid white",
+//   borderRadius: "none",
+//   padding: "2rem 3rem",
+//   fontSize: "xl",
+//   fontWeight: "bold",
+//   letterSpacing: "0.25ch",
+//   _hover: {
+//     background: "white",
+//     color: "black",
+//     border: "2px solid white",
+//   },
+// });
+
+// export const buttonTheme = defineStyleConfig({
+//   variants: { brandPrimaryOnLight, brandPrimaryOnDark },
+// });
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'League Spartan', sans-serif`,
+    body: `'League Spartan', sans-serif`,
+  },
+  //   components: {
+  //     Button: buttonTheme,
+  //   },
+  //   breakpoints,
+});
+
+export default theme;
