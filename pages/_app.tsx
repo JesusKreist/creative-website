b9ed9e60bfba81077fe9266015eaac75accbed81
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-29 20:47:29 +0100

    Add font weights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import "../styles/globals.css";
-import "@fontsource/commissioner";
+import "@fontsource/commissioner/100.css";
+import "@fontsource/commissioner/200.css";
+import "@fontsource/commissioner/300.css";
+import "@fontsource/commissioner/400.css";
+import "@fontsource/commissioner/500.css";
+import "@fontsource/commissioner/600.css";
+import "@fontsource/commissioner/700.css";
+import "@fontsource/commissioner/800.css";
+import "@fontsource/commissioner/900.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../styles/theme";
