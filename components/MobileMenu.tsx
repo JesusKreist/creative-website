f9797bfcd5f520eb559e5b8242bf9ee4e922410c
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-29 17:07:24 +0100

    Add MobileMenu to HomePage component

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
+
+interface MobileMenuProps {
+  isMobileOpen: boolean;
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ isMobileOpen }) => (
+  <Box
+    display={{ base: isMobileOpen ? "flex" : "none", md: "none" }}
+    width="252px"
+    height="292px"
+    position="absolute"
+    bgColor="brand.bgBlack"
+    color="white"
+    right="0"
+  >
+    <List
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      margin="0 auto"
+      gap="2rem"
+      flexDirection="column"
+    >
+      <Link href="/about">
+        <ListItem>About</ListItem>
+      </Link>
+      <Link href="/service">
+        <ListItem>Service</ListItem>
+      </Link>
+      <Link href="/projects">
+        <ListItem>Projects</ListItem>
+      </Link>
+      <Link href="/schedule-call">
+        <ListItem>
+          <Button
+            width="199px"
+            height="64px"
+            variant={{ base: "brandRedSmall", xl: "brandRedBig" }}
+          >
+            Schedule a Call
+          </Button>
+        </ListItem>
+      </Link>
+    </List>
+  </Box>
+);
+
+export default MobileMenu;
