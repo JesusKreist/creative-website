6adc267f8123a03b38d390c38089178ef0d56c07
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-30 21:04:38 +0100

    Refactor OurApproachText component as a grid

--- components/Showcase/OurApproachText.tsx
+++ components/Showcase/OurApproachText.tsx
@@ -1,12 +1,12 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, Text } from "@chakra-ui/react";
 
 const OurApproachText = () => {
   return (
-    <Box
+    <Grid
       overflow="hidden"
       className="approach"
-      alignSelf="center"
-      justifySelf="end"
+      // alignSelf="center"
+      // justifySelf="end"
       gridColumn={{
         md: "8 / -1",
         lg: "1 / span 8",
@@ -16,13 +16,21 @@ const OurApproachText = () => {
         lg: "span 11 / -1",
       }}
       zIndex="4"
+      templateRows={{ lg: "repeat(22, 1fr)" }}
+      templateColumns={{ lg: "repeat(16, 1fr)" }}
     >
-      <Flex width="540px" height="323px" color="black">
+      <Box
+        gridRow="7 / span 3"
+        gridColumn="3 / span 2"
+        width="540px"
+        height="192px"
+        color="black"
+      >
         <Text fontSize="3.5rem" lineHeight="4rem" fontWeight="extrabold">
           Our approach for creating a winning brand
         </Text>
-      </Flex>
-    </Box>
+      </Box>
+    </Grid>
   );
 };
 
