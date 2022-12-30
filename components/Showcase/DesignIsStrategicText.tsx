4146adeeda638b151627c1f7f3de854d832418fa
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-30 20:02:04 +0100

    Refactor DesignIsStrategic component as a grid

--- components/Showcase/DesignIsStrategicText.tsx
+++ components/Showcase/DesignIsStrategicText.tsx
@@ -1,9 +1,17 @@
-import { Box, Button, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  Image,
+  Grid,
+} from "@chakra-ui/react";
 import React from "react";
 
 const DesignIsStrategicText = () => {
   return (
-    <Box
+    <Grid
       className="strategic-design"
       gridColumn={{
         md: "8 / -1",
@@ -11,15 +19,6 @@ const DesignIsStrategicText = () => {
         xl: "span 9 / -1",
         "2xl": "9 / -1",
       }}
-      //   gridRow={{
-      //     md: "8 / -1",
-      //     lg: "span 10 / -1",
-      //     xl: "span 9 / -1",
-      //     "2xl": "1 / span 13",
-      //   }}
-      //   width="705px"
-      //   height="784px"
-
       gridRow={{
         md: "8 / -1",
         lg: "span 10 / -1",
@@ -28,53 +27,66 @@ const DesignIsStrategicText = () => {
       }}
       bgColor="brand.bgBlack"
       zIndex="1"
+      templateColumns={{ "2xl": "repeat(18, 1fr)" }}
+      position="relative"
     >
-      <Flex width="666px" height="100%" alignItems="center" marginLeft="-4rem">
-        <Flex gap="7rem">
+      <Box width="135px" position="absolute" top="170px" left="-70px">
+        <Image src="/desktop/bg-pattern-wave-red.svg" alt=""></Image>
+      </Box>
+      <Flex
+        width="445px"
+        color="white"
+        direction="column"
+        gap="3.5rem"
+        alignSelf="center"
+        gridColumn={{ "2xl": "4 / span 2" }}
+      >
+        <Heading
+          fontSize={{ base: "2.5rem", md: "3.5rem" }}
+          lineHeight={{ base: "2.5rem", md: "3.5rem", xl: "4rem" }}
+          fontWeight="extrabold"
+        >
+          <Box as="span" color="brand.red">
+            Design
+          </Box>{" "}
+          is strategic
+        </Heading>
+        <Text
+          fontSize={{ lg: "1rem", xl: "1.125rem" }}
+          lineHeight={{ lg: "1.625rem", xl: "2rem" }}
+          fontWeight="normal"
+        >
+          “A well-crafted design strategy consistently produces desired outcomes
+          and brand awareness. We are firm believers that success lies in
+          creative collaboration with our clients.”
+        </Text>
+        <Box alignSelf="start">
+          <Button
+            variant="unstyled"
+            fontSize="1.125rem"
+            color="brand.red"
+            borderRadius="none"
+            borderBottom="1px solid red"
+            marginBlock="1rem"
+            _hover={{
+              color: "orange",
+              borderBottom: "1px solid orange",
+            }}
+          >
+            Schedule a Call
+          </Button>
+        </Box>
+      </Flex>
+      {/* <Flex width="666px" height="100%" alignItems="center" marginLeft="-4rem">
+        <Flex gap="4rem">
           <Box width="135px">
             <Image src="/desktop/bg-pattern-wave-red.svg" alt=""></Image>
           </Box>
 
-          <Flex width="445px" color="white" direction="column" gap="3.5rem">
-            <Heading
-              fontSize={{ base: "2.5rem", md: "3.5rem" }}
-              lineHeight={{ base: "2.5rem", md: "3.5rem", xl: "4rem" }}
-              fontWeight="extrabold"
-            >
-              <Box as="span" color="brand.red">
-                Design
-              </Box>{" "}
-              is strategic
-            </Heading>
-            <Text
-              fontSize={{ lg: "1rem", xl: "1.125rem" }}
-              lineHeight={{ lg: "1.625rem", xl: "2rem" }}
-              fontWeight="normal"
-            >
-              “A well-crafted design strategy consistently produces desired
-              outcomes and brand awareness. We are firm believers that success
-              lies in creative collaboration with our clients.”
-            </Text>
-            <Box alignSelf="start">
-              <Button
-                variant="unstyled"
-                fontSize="1.125rem"
-                color="brand.red"
-                borderRadius="none"
-                borderBottom="1px solid red"
-                marginBlock="1rem"
-                _hover={{
-                  color: "orange",
-                  borderBottom: "1px solid orange",
-                }}
-              >
-                Schedule a Call
-              </Button>
-            </Box>
-          </Flex>
+          
         </Flex>
-      </Flex>
-    </Box>
+      </Flex> */}
+    </Grid>
   );
 };
 
