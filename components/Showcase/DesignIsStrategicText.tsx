7b176fd5a061614717762490dcdb614bd6beeb9e
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-30 16:26:09 +0100

    Add component to display the features of the agency

--- components/Showcase/DesignIsStrategicText.tsx
+++ components/Showcase/DesignIsStrategicText.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text, Image } from "@chakra-ui/react";
 import React from "react";
 
 const DesignIsStrategicText = () => {
@@ -11,16 +11,75 @@ const DesignIsStrategicText = () => {
         xl: "span 9 / -1",
         "2xl": "9 / -1",
       }}
+      //   gridRow={{
+      //     md: "8 / -1",
+      //     lg: "span 10 / -1",
+      //     xl: "span 9 / -1",
+      //     "2xl": "1 / span 13",
+      //   }}
+      //   width="705px"
+      //   height="784px"
+
       gridRow={{
         md: "8 / -1",
         lg: "span 10 / -1",
         xl: "span 9 / -1",
-        "2xl": "1 / span 13",
+        "2xl": "1 / span 5",
       }}
-      //   width="705px"
-      //   height="784px"
       bgColor="brand.bgBlack"
-    ></Box>
+    >
+      <Flex
+        width="666px"
+        gap="8rem"
+        height="100%"
+        alignItems="center"
+        marginLeft="-4rem"
+      >
+        <Flex gap="6rem">
+          <Box width="135px">
+            <Image src="/desktop/bg-pattern-wave-red.svg" alt=""></Image>
+          </Box>
+
+          <Flex width="445px" color="white" direction="column" gap="3.5rem">
+            <Heading
+              fontSize={{ base: "2.5rem", md: "3.5rem" }}
+              lineHeight={{ base: "2.5rem", md: "3.5rem", xl: "4rem" }}
+              fontWeight="extrabold"
+            >
+              <Box as="span" color="brand.red">
+                Design
+              </Box>{" "}
+              is strategic
+            </Heading>
+            <Text
+              fontSize={{ lg: "1rem", xl: "1.125rem" }}
+              lineHeight={{ lg: "1.625rem", xl: "2rem" }}
+              fontWeight="normal"
+            >
+              “A well-crafted design strategy consistently produces desired
+              outcomes and brand awareness. We are firm believers that success
+              lies in creative collaboration with our clients.”
+            </Text>
+            <Box alignSelf="start">
+              <Button
+                variant="unstyled"
+                fontSize="1.125rem"
+                color="brand.red"
+                borderRadius="none"
+                borderBottom="1px solid red"
+                marginBlock="1rem"
+                _hover={{
+                  color: "orange",
+                  "border-bottom": "1px solid orange",
+                }}
+              >
+                Schedule a Call
+              </Button>
+            </Box>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
 
--- components/Showcase/ThingsWeDo.tsx
+++ components/Showcase/ThingsWeDo.tsx
@@ -1,4 +1,49 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+
+interface FeatureProps {
+  heading: string;
+  text: string;
+  paddedNumber: string;
+}
+const Feature: React.FC<FeatureProps> = ({ paddedNumber, heading, text }) => {
+  return (
+    <Flex
+      overflow="hidden"
+      color="white"
+      width="504px"
+      height="196px"
+      justifyContent="center"
+      position="relative"
+      paddingLeft="3.5rem"
+    >
+      <Flex
+        width="445px"
+        direction="column"
+        justifyContent="end"
+        gap="1.5rem"
+        zIndex="1"
+      >
+        <Heading fontSize="1.25rem" fontWeight="extrabold" lineHeight="2rem">
+          {heading}
+        </Heading>
+        <Text fontSize="1.125rem" lineHeight="2rem">
+          {text}
+        </Text>
+      </Flex>
+
+      <Text
+        position="absolute"
+        fontSize="5rem"
+        fontWeight="extrabold"
+        lineHeight="5.5rem"
+        left="0"
+        opacity="0.25"
+      >
+        {paddedNumber}
+      </Text>
+    </Flex>
+  );
+};
 
 const ThingsWeDo = () => {
   return (
@@ -10,16 +55,47 @@ const ThingsWeDo = () => {
         xl: "span 9 / -1",
         "2xl": "span 11 / -1",
       }}
+      //   gridRow={{
+      //     md: "8 / -1",
+      //     lg: "span 10 / -1",
+      //     xl: "span 9 / -1",
+      //     "2xl": "span 19 / -1",
+      //   }}
+
       gridRow={{
         md: "8 / -1",
         lg: "span 10 / -1",
         xl: "span 9 / -1",
-        "2xl": "span 19 / -1",
+        "2xl": "span 14 / -1",
       }}
       bgColor="brand.red"
       //   width="895px"
-      //   height="1100px"
-    ></Box>
+      zIndex="3"
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        justify="center"
+        height="100%"
+        gap="4rem"
+      >
+        <Feature
+          heading="Brand Strategy"
+          text="Brand strategy is critical for long-term success. Outshining competitors and capturing the target audience are key."
+          paddedNumber="01"
+        />
+        <Feature
+          heading="Brand Design"
+          text="Keeping the brand design unique and meaningful helps in communicating the brand's timeless value effectively."
+          paddedNumber="02"
+        />
+        <Feature
+          heading="Web Design"
+          text="A beautifully crafted website is the best tool for brand awareness, and ultimately results in increased revenues. "
+          paddedNumber="03"
+        />
+      </Flex>
+    </Box>
   );
 };
 
