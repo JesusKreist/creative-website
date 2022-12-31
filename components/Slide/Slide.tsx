2de9944bbcbe1beaa7e3906a1dbee2db2e8970e2
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-31 20:19:23 +0100

    Move background image of slide component to separate component

--- components/Slide/Slide.tsx
+++ components/Slide/Slide.tsx
@@ -10,24 +10,14 @@ import {
 import React from "react";
 import { RxChevronLeft, RxChevronRight } from "react-icons/rx";
 import SlideContainer from "./SlideContainer";
+import SlideImage from "./SlideImage";
 import SlideText from "./SlideText";
 
 const Slide = () => {
   return (
     <SlideContainer>
       <SlideText />
-
-      <Box
-        gridColumn={{
-          base: "1 / span 16",
-          md: "span 14 / -1",
-          lg: "span 11 / -1",
-        }}
-        gridRow="1 / -1"
-        bgImage="/desktop/image-slide-1.jpg"
-        bgSize="cover"
-        bgRepeat="no-repeat"
-      ></Box>
+      <SlideImage />
     </SlideContainer>
   );
 };
--- components/Slide/SlideImage.tsx
+++ components/Slide/SlideImage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
 
 const SlideImage = () => {
-  return <div>SlideImage</div>;
+  return (
+    <Box
+      gridColumn={{
+        base: "1 / span 16",
+        md: "span 14 / -1",
+        lg: "span 11 / -1",
+      }}
+      gridRow="1 / -1"
+      bgImage="/desktop/image-slide-1.jpg"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+    ></Box>
+  );
 };
 
 export default SlideImage;
