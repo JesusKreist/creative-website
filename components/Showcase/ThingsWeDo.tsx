6fa916eec6e887e143c4f4338d20f3a5205ee49e
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-05 18:25:20 +0100

    Add animations to the ThingsWeDo component

--- components/Showcase/ThingsWeDo.tsx
+++ components/Showcase/ThingsWeDo.tsx
@@ -1,11 +1,35 @@
 import { Box, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+const childVariants = {
+  start: { opacity: 0 },
+  end: { opacity: 1, transition: { duration: 1 } },
+};
 
 interface FeatureProps {
   heading: string;
   text: string;
   paddedNumber: string;
+  transitionDelay: number;
 }
-const Feature: React.FC<FeatureProps> = ({ paddedNumber, heading, text }) => {
+const Feature: React.FC<FeatureProps> = ({
+  paddedNumber,
+  heading,
+  text,
+  transitionDelay,
+}) => {
+  const boxRef = useRef(null);
+  const isInView = useInView(boxRef, { once: true });
+
+  const animationVariant = {
+    ...childVariants,
+    end: {
+      ...childVariants.end,
+      transition: { ...childVariants.end.transition, delay: transitionDelay },
+    },
+  };
+
   return (
     <Flex
       color="white"
@@ -14,8 +38,13 @@ const Feature: React.FC<FeatureProps> = ({ paddedNumber, heading, text }) => {
       justifyContent="center"
       position="relative"
       paddingLeft={{ base: "2.5rem", md: "3.5rem" }}
+      ref={boxRef}
     >
       <Flex
+        as={motion.div}
+        variants={animationVariant}
+        initial="start"
+        animate={isInView ? "end" : ""}
         maxWidth={{ base: "400px", md: "445px" }}
         direction="column"
         justifyContent="end"
@@ -83,16 +112,19 @@ const ThingsWeDo = () => {
           heading="Brand Strategy"
           text="Brand strategy is critical for long-term success. Outshining competitors and capturing the target audience are key."
           paddedNumber="01"
+          transitionDelay={0}
         />
         <Feature
           heading="Brand Design"
           text="Keeping the brand design unique and meaningful helps in communicating the brand's timeless value effectively."
           paddedNumber="02"
+          transitionDelay={0.5}
         />
         <Feature
           heading="Web Design"
           text="A beautifully crafted website is the best tool for brand awareness, and ultimately results in increased revenues. "
           paddedNumber="03"
+          transitionDelay={1}
         />
       </Flex>
     </Grid>
